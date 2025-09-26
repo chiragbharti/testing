56ebc3c22e7f7370321c827bf535c5cd7910b2df
chiragbharti <100470541+chiragbharti@users.noreply.github.com>
2025-09-26 16:26:22 +0530

    make login route
    make login route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var con = require('../server/connection');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.post('/login', function(req, res, next) {
+  console.log(req.body);
+});
+
 
 module.exports = router;
